5e278a15a763db4a1b66c9b2d9cebdb10cbb7af8
Aleksei <alekseisligai@gmail.com>
2023-10-24 16:53:56 +0600

    Concluded conditionals

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if(divider != 0) {
+            int res = dividend / divider;
+            System.out.println(dividend == divider * res ? "can be divided completely" : "cannot be divided completely");
+        } else {
+            System.out.println("division by zero");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        if (firstSide >= secondSide + thirdSide) {
+            System.out.println("it's not a triangle");
+        } else if (secondSide >= firstSide + thirdSide) {
+            System.out.println("it's not a triangle");
+        } else if (thirdSide >= firstSide + secondSide) {
+            System.out.println("it's not a triangle");
+        } else {
+            System.out.println("this is a valid triangle");
+        }
 
     }
 }
